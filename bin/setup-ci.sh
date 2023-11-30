a596c04e6a6a612e53a63b999e82e1c0db5365ec
Mariano Acebal <macebal@outlook.com.ar>
2023-11-30 10:04:33 -0300

    fix: changes to setup-ci

--- bin/setup-ci.sh
+++ bin/setup-ci.sh
@@ -1,7 +1,3 @@
 #The following commands are related to pyautogui installation
-apt-get install scrot
-apt-get install python3-tk
-apt-get install python3-dev
-
-# xhost +
-# touch ~/.Xauthority
+xhost +
+touch ~/.Xauthority
