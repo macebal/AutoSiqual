b51a54e7d430f9a982e41bfbbf2eaad236e343f4
Mariano Acebal <macebal@outlook.com.ar>
2023-11-30 10:03:26 -0300

    chore: removed sudo

--- bin/setup-ci.sh
+++ bin/setup-ci.sh
@@ -1,7 +1,7 @@
 #The following commands are related to pyautogui installation
-sudo apt-get install scrot
-sudo apt-get install python3-tk
-sudo apt-get install python3-dev
+apt-get install scrot
+apt-get install python3-tk
+apt-get install python3-dev
 
 # xhost +
 # touch ~/.Xauthority
