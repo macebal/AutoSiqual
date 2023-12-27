c92a4858157274b708a6a6ef9ed72dc8b5f07a96
Mariano Acebal <macebal@outlook.com.ar>
2023-12-27 20:43:43 -0300

    chore: moved `main.py` and added colors to build script

--- bin/build.sh
+++ bin/build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
 #Delete previous files
 rm -r dist/
 
 #Run pyinstaller to create the executables in the dist/folder
-pyinstaller main.py --onefile --noconsole
+pyinstaller src/main.py --onefile --noconsole
 
 #copy the config.json file and the img/ folder to the dist folder
 cp config.json dist/config.json
@@ -15,9 +20,9 @@ new_filename="AutoSiqual v$(poetry version -s)"
 if [ -f "dist/main.exe" ]; then
     # Rename 'main.exe' to the new filename
     mv "dist/main.exe" "dist/$new_filename.exe"
-    echo "Renamed main.exe to $new_filename.exe"
+    echo -e "${YELLOW}Renamed main.exe to $new_filename.exe${NC}"
 else
-    echo "Error: dist/main.exe not found."
+    echo -e "${RED}Error: dist/main.exe not found.${NC}"
 fi
 
 # Zip everything in 'dist/' directory into 'new_filename.zip'
@@ -25,10 +30,10 @@ fi
 # https://stackoverflow.com/questions/38782928/how-to-add-man-and-zip-to-git-bash-installation-on-windows
 if [ -d "dist" ]; then
     (cd "dist" && zip -r "${new_filename}.zip" ./*)
-    echo "Zipped 'dist/' contents into '${new_filename}.zip'"
+    echo -e "${YELLOW}Zipped 'dist/' contents into '${new_filename}.zip'${NC}"
 else
     echo "Error: 'dist/' directory not found."
     exit 1
 fi
 
-echo "dist/${new_filename}.zip is ready to distribute. If the zip didn't get built, see bin/build.sh"
+echo -e "${GREEN}dist/${new_filename}.zip is ready to distribute. If the zip didn't get built, see bin/build.sh${NC}"
